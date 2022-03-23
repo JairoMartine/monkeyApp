fddcffb5069642135309ca2c3fb2902bae180258
Jairo Emmanuel Martinez Olvera <emmanuelmo.ti19@utsjr.edu.mx>
2022-03-22 19:25:48 -0600

    Subida Uno

---
+++ lib/src/routes/routes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:monkeyapp/main.dart';
+import 'package:monkeyapp/src/pages/home_page.dart';
+
+
+final routes = <String, WidgetBuilder>{
+  '/':(BuildContext context)=>const HomePage()
+};
